fee16314d7fbeb0f020c411ada5342771c0c380f
Floris den Hengst <florisdenhengst@gmail.com>
2019-09-03 12:01:36 +0200

    alpha version

--- create_cards.py
+++ create_cards.py
@@ -2,24 +2,29 @@ import random
 import csv
 import math
 
-numbersheets = 20
+numbersheets = 300
 gridsize = 5
 stepping = 16
 save_path = './cards.html'
 input_file = './input.csv'
-center_figure = '*'
+center_figure = '<img src="https://raw.githubusercontent.com/florisdenhengst/music-bingo/master/center-img-small.png"/>'
 
 input_words = []
+input_ids = []
 with open(input_file, 'r') as f:
     r = csv.reader(f, delimiter=',', quotechar='"')
+    _ = next(r)
     for row in r:
         input_words.append(row[0])
+        input_ids.append(row[1])
 
 print('Creating bingo cards for: ' + str(len(input_words)) + ' words')
 n_words = len(input_words)
 words = []
 for i, w in enumerate(input_words):
-    words.append('{}: {}'.format(i, w))
+    words.append('{}: {}'.format(input_ids[i], w))
+
+sheets = set()
 
 with open(save_path, 'w') as f:
     f.write("<html>")
@@ -29,12 +34,21 @@ with open(save_path, 'w') as f:
             td {
 		width: 120px;
 		height: 50px;
-		padding: 10px;
+		padding: 2px;
+                overflow: hidden;
 		text-align: center;
 		vertical-align: middle;
 		border: 1px solid black;
-                font-size: 10pt;
+                font-size: 9pt;
                 font-family: Arial, Helvetica, sans-serif;
+            }
+            img {
+                max-height: 50px;
+            }
+            @media print{
+                br.page{
+                    page-break-before: always;
+                }
             }
         </style>
     </head>
@@ -45,7 +59,15 @@ with open(save_path, 'w') as f:
         f.write('\n')
 
         bingosheet = []
-        sheet = random.sample(words, gridsize**2 - 1)
+        duplicate = True
+        while duplicate:
+            sheet = random.sample(words, gridsize**2 - 1)
+            o_sheet = '~'.join(sheet)
+            duplicate = o_sheet in sheets
+            if duplicate:
+                print('Duplicate, retrying')
+        sheets.add(o_sheet)
+
         sheet.insert(math.ceil(len(sheet)/2), center_figure)
 
         f.write("<table>")
@@ -64,4 +86,8 @@ with open(save_path, 'w') as f:
                 f.write(columnstring)
             f.write("</tr>\n")
         f.write("</table>")
+        if (i+1) % 2 == 0:
+            f.write("<br class='page'/>")
+        else:
+            f.write("<br/>")
         f.write("\n")
---
+++ randno.py
@@ -0,0 +1,6 @@
+import random
+
+rands = random.sample(range(1, 161), 160)
+
+for i in rands:
+    print(i)
